d2ab5cbec299180f262f7da27abea7c3e73fa7cd
Low Memory <fernando+oasis@demo-tic.org>
2015-12-18 16:50:26 +0100

    Handle 2nd level menus

--- create_TOC.sh
+++ create_TOC.sh
@@ -7,16 +7,24 @@ temp_file=temp_mydocs.yml
 input_toc=seafile-docs/docs/SUMMARY.md
 
 # Set name
-echo "site_name: Server Seafile Manual\n" > $output_toc
-echo "\npages:" >> $output_toc
+echo "site_name: Server Seafile Manual\n" > ${output_toc}
+
+echo "\npages:" >> ${output_toc}
 
 # Change current md TOC to mydocs
-sed 's/^* \[\(.*\)\](\(.*\))/\- '\''\1'\'':\n   \- \2/g' < $input_toc > $temp_file
-sed 's/* \[\(.*\)\](\(.*\))/\- '\''\1'\'': '\''\2'\''/g' < $temp_file >> $output_toc
 
-#sed 's/^* \[(*.)/- '\''/g
-#s/* \[/- '\''/g
-#s/\](/'\'': '\''/g
-#s/)/'\''/g' < $input_toc >> $output_toc
+# "First level menus"
+sed 's/^* \[\(.*\)\](\(.*\))/\- '\''\1'\'':\n   \- \2/g' < ${input_toc} > ${temp_file}
 
+# "Second level menus"
+sed -i 's/^   * \[\(.*\)\](\(.*\))/\   - '\''\1'\'':\n   \- \2/g' ${temp_file}
+
+# "Third level menus"
+sed 's/^       \* \[\(.*\)\](\(.*\))/\       - '\''\1'\'': \2/g' < ${temp_file} >> ${output_toc}
+
+# add theme
 echo "\ntheme: readthedocs" >> $output_toc
+
+# remove temporary files
+rm ${temp_file}
+
